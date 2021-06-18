85ce4faefcef85642427a0698b55125f0a55ecbf
chandan-9606 <85636455+chandan-9606@users.noreply.github.com>
2021-06-18 19:59:18 +0530

    The Developed by Chandan Python Programme
    The Problem is Python Programming in Positive  Numbers

---
+++ PositiveNumbers.py
@@ -0,0 +1,26 @@
+#include<studio.h>
+list1 = [12, -7, 5, 64,  -14 ] 
+list2=[12, 14, -95, 3 ]
+
+for num in list1: 
+
+    if num >= 0:
+        print(num, end = " ") 
+       
+Output : 12, 5, 64
+            
+ #include<studio.h>
+ list2=[12, 14, -95, 3]
+ 
+ for num in list2:
+    
+    if num >= 0:
+        print(num,end = " ")
+        
+Output : 12, 14, 3
+        
+                     
+ 
+ 
+ 
+
